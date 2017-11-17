d33bf3f046056d99f986a6c877f08b891bf713a8
James Lee <jamesusa19@yahoo.com>
2017-11-16 16:14:10 -0800

    Used angularjs filter to sort names

--- root.js
+++ root.js
@@ -2,5 +2,5 @@ let app = angular.module('root', []);
 
 app.controller('index', ['$scope', function($scope){
 	$scope.message = 'Hello World';
-	$scope.names = ['Jane', 'Jeff', 'Joey'];
+	$scope.names = ['Jane', 'Jeff', 'Joey', 'Amy'];
 }]);
