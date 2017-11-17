036aa755a306e39eb41b3e4e116b8702bd1dbffc
James Lee <jamesusa19@yahoo.com>
2017-11-17 10:48:16 -0800

    Used button onclick to add to names object via ngModel input attributes

--- root.js
+++ root.js
@@ -2,5 +2,11 @@ let app = angular.module('root', []);
 
 app.controller('index', ['$scope', function($scope){
 	$scope.message = 'Hello World';
-	$scope.names = ['Jane', 'Jeff', 'Joey', 'Amy'];
+	$scope.names = [];
+	$scope.add = function(){
+		$scope.names.push({fname: $scope.fname, lname: $scope.lname})
+	}
+	$scope.listName = function(fname, lname){
+		return fname + " " + lname;
+	}
 }]);
