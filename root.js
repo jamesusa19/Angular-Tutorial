c31d49818b6cb388b77d9ab00797f9b7d16f77d1
James Lee <jamesusa19@yahoo.com>
2017-11-21 17:44:08 -0800

    refactored code a bit to follow tutorial, worked on adding elements to list via form and ng-submit

--- root.js
+++ root.js
@@ -1,16 +1,14 @@
 let app = angular.module('root', [])
     .controller('index', ['$scope', function($scope){
-        $scope.message = 'Hello World';
-        $scope.names = [];
-        $scope.add = function(){
-            $scope.names.push({fname: $scope.fname, lname: $scope.lname})
-        }
-        $scope.listName = function(fname, lname){
-            return fname + " " + lname;
-        }
         $scope.list = [{name:'Scott', age: 29},
                        {name:'James',age:22},
                        {name:'Ross',age:31},
                        {name:'Ben',age:39},
                        {name:'Kevin',age:19}];
+                       
+        $scope.addPerson = function(){
+            $scope.list.push({name: $scope.name, age: $scope.age});
+            $scope.name = '';
+            $scope.age = 0;
+        };
     }]);
