f32323eaabfc2bdcc9477bb1d44f31e6d3ae99b5
James Lee <jamesusa19@yahoo.com>
2017-11-21 17:33:52 -0800

    added a controller

--- root.js
+++ root.js
@@ -1,12 +1,16 @@
-let app = angular.module('root', []);
-
-app.controller('index', ['$scope', function($scope){
-	$scope.message = 'Hello World';
-	$scope.names = [];
-	$scope.add = function(){
-		$scope.names.push({fname: $scope.fname, lname: $scope.lname})
-	}
-	$scope.listName = function(fname, lname){
-		return fname + " " + lname;
-	}
-}]);
+let app = angular.module('root', [])
+    .controller('index', ['$scope', function($scope){
+        $scope.message = 'Hello World';
+        $scope.names = [];
+        $scope.add = function(){
+            $scope.names.push({fname: $scope.fname, lname: $scope.lname})
+        }
+        $scope.listName = function(fname, lname){
+            return fname + " " + lname;
+        }
+        $scope.list = [{name:'Scott', age: 29},
+                       {name:'James',age:22},
+                       {name:'Ross',age:31},
+                       {name:'Ben',age:39},
+                       {name:'Kevin',age:19}];
+    }]);
